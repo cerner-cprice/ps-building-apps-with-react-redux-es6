99531571e40661a70e9ce3a4df822a18af05ec1c
Cody Price <Cody.Price@cerner.com>
2017-11-22 12:04:57 -0600

    mod11: async writes in redux

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -3,10 +3,13 @@ import { connect } from 'react-redux';
 import * as courseActions from '../../actions/courseActions';
 import { bindActionCreators } from 'redux';
 import CourseList from './CourseList';
+import { browserHistory } from 'react-router';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
+
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
   }
 
   courseRow(course, index) {
@@ -15,12 +18,22 @@ class CoursesPage extends React.Component {
     );
   }
 
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
+  }
+
   render() {
     const {courses} = this.props;
 
     return (
       <div>
         <h1>Courses</h1>
+        <input
+          type="submit"
+          value="Add Course"
+          className="btn btn-primary"
+          onClick={this.redirectToAddCoursePage}
+        />
         <CourseList courses={courses} />
       </div>
     );
